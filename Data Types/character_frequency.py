c934d3806ec7770cd0cbcb8c142f0db980afdcec
Avhi Aryal (Deepak Aryal) <avhiaryal54@gmail.com>
2021-02-14 01:14:01 +0545

    character's frequency

---
+++ Data Types/character_frequency.py
@@ -0,0 +1,11 @@
+sample_string ="google.com"
+
+frequency = {}
+
+for i in sample_string:
+    if i in frequency:
+        frequency[i] += 1
+    else:
+        frequency[i] = 1
+
+print ("frequency of all characters in string "+sample_string+" is :\n" + str(frequency))
