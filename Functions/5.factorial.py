3ad6e8ad383319e9e5d5e9413bcc5ad8a8685a99
Avhi Aryal (Deepak Aryal) <avhiaryal54@gmail.com>
2021-02-14 17:24:44 +0545

    factorial

---
+++ Functions/5.factorial.py
@@ -0,0 +1,8 @@
+#python function to calculate the factorial of a number
+def fact(n):
+    if n==0:
+        return 1
+    else:
+        return n * fact(n-1)
+n = int(input("Enter the number to find its factorial: "))
+print (fact(n))
