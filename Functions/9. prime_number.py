dc9e19f6983051ca1dd4a26b8dc403895cbe39cd
Avhi Aryal (Deepak Aryal) <avhiaryal54@gmail.com>
2021-02-14 21:32:00 +0545

    check prime number

---
+++ Functions/9. prime_number.py
@@ -0,0 +1,16 @@
+#python function that takes a number as a parameter and check the number is prime or not
+def prime(n):
+    if (n==1):
+        return False
+    elif (n==2):
+        return True
+    else:
+        for x in range (2,n):
+            if (n % x ==0):
+                return False
+            return True
+print(prime(13))
+
+
+
+            
