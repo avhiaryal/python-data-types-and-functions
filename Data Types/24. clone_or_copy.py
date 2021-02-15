790b0ae5786841410f1ff6582395e3d1c1039970
Avhi Aryal (Deepak Aryal) <avhiaryal54@gmail.com>
2021-02-15 20:50:31 +0545

    clone or copy a list

---
+++ Data Types/24. clone_or_copy.py
@@ -0,0 +1,5 @@
+# python program to clone or copy a list
+given_list = [5,10,15,20,25]
+cloned_list = list(given_list)
+print("The given list is :",given_list)
+print("The clone of given list is :",cloned_list)
