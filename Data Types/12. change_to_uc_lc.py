765317026560541a781cb85ea6be68739812f2f2
Avhi Aryal (Deepak Aryal) <avhiaryal54@gmail.com>
2021-02-15 11:00:26 +0545

    Create 12. change_to_uc_lc.py

---
+++ Data Types/12. change_to_uc_lc.py
@@ -0,0 +1,4 @@
+#python program to takes input from the user and displays that input back in upper case an lower cases
+favourite_movie = input("What is your favourite movie? ")
+print(favourite_movie.upper())
+print(favourite_movie.lower())
