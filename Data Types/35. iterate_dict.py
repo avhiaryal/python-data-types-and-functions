7a47402f9503fdc9f681268f439be97285ba7be5
Avhi Aryal (Deepak Aryal) <avhiaryal54@gmail.com>
2021-02-15 22:49:33 +0545

    iterate over dictionaries using for loops

---
+++ Data Types/35. iterate_dict.py
@@ -0,0 +1,5 @@
+# python program to iterate over dictionaries using for loops
+
+dict1 = {'name':'deepak','age':23,'address':'kathmandu'}
+for dict1_key, dict1_value in dict1.items():
+    print(dict1_key,'=',dict1_value)
