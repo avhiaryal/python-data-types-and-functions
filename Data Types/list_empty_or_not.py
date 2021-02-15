2734fb36e8a5ff1dd4473e422be937f7e13b4dbf
Avhi Aryal (Deepak Aryal) <avhiaryal54@gmail.com>
2021-02-15 20:46:28 +0545

    check if the list is empty or not

---
+++ Data Types/list_empty_or_not.py
@@ -0,0 +1,13 @@
+#python program to check a list is empty or not
+
+list1 = []
+list2 = [0]
+if not list1:
+    print('list 1 is empty List')
+else:
+    print("list 1 is not empty")
+if not list2:
+    print ('list 2 is empty  list')
+else:
+    print('list 2 is not empty list')
+    
