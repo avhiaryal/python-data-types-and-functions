d58952998efc235c1118d60a937b79b7bea286dc
Avhi Aryal (Deepak Aryal) <avhiaryal54@gmail.com>
2021-02-15 22:51:02 +0545

    add item in a tuple

---
+++ Data Types/40. addintuple.py
@@ -0,0 +1,9 @@
+# python program to add an item in a tuple
+
+tuple1 = (1,2,3,4,5)
+print (tuple1)
+
+#since tuple are immutable, new elements cannot be added
+# adding element in tuple using different method
+tuple1 = tuple1 + (6,7)
+print(tuple1)
