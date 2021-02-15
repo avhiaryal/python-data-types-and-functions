c9fd942448ea2f32f94ec9f6734262fc14e31c97
Avhi Aryal (Deepak Aryal) <avhiaryal54@gmail.com>
2021-02-15 21:35:02 +0545

    print dictionary (x,x**2)

---
+++ Data Types/33. print_dict2.py
@@ -0,0 +1,6 @@
+# python program to print a dictionary where the keys are numbers between 1 and 15
+# (both included) and the values are square of keys
+dict1= dict()
+for a in range(1,16):
+    dict1[a]=a**2
+print(dict1)
