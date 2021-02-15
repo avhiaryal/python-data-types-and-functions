96bdd759562c53af954ad1df7857b870689bd5f3
Avhi Aryal (Deepak Aryal) <avhiaryal54@gmail.com>
2021-02-15 14:31:01 +0545

    lamda function to add and multiply

---
+++ Functions/11. lamda_problem.py
@@ -0,0 +1,9 @@
+# program to create a lambda function that adds 15 to a given number passes in as an argument, 
+# also create a lambda function that multiplies argument x with argument y and print the result.
+
+l = lambda d : d + 15
+print (l(20))
+
+l = lambda x, y : x * y
+print (l(10,15))
+ 
